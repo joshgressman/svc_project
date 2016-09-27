b6cee975a5642889bd1e32513241175554bfb69b
kmaimn <kmai.mn@gmail.com>
2016-09-26 19:03:48 -0500

    skeleton for GET requests

--- server/routes/reportRoute.js
+++ server/routes/reportRoute.js
@@ -11,11 +11,91 @@ if (process.env.DATABASE_URL != undefined) {
 }
 
 //GET for federal report;
+router.get('/federal', function (req, res) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      console.log('ERROR, connection to PG', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT...', function (err, result) {
+      done();
+
+      if (err) {
+        console.log('GET ERROR, federal:', err);
+        res.sendStatus(500);
+      }
+
+      console.log('Federal Report:', result.rows);
+      res.send(result.rows);
+    });
+  });
+});
 
 //GET for hennepin county report;
+router.get('/hennepin', function (req, res) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      console.log('ERROR, connection to PG', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT...', function (err, result) {
+      done();
+
+      if (err) {
+        console.log('GET ERROR, hennepin:', err);
+        res.sendStatus(500);
+      }
+
+      console.log('Hennepin County Report:', result.rows);
+      res.send(result.rows);
+    });
+  });
+});
 
 //GET for summary report;
+router.get('/summary', function (req, res) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      console.log('ERROR, connection to PG', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT...', function (err, result) {
+      done();
+
+      if (err) {
+        console.log('GET ERROR, summary:', err);
+        res.sendStatus(500);
+      }
+
+      console.log('Summary Report:', result.rows);
+      res.send(result.rows);
+    });
+  });
+});
 
 //GET for data playground report;
+router.get('/playground', function (req, res) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      console.log('ERROR, connection to PG', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT...', function (err, result) {
+      done();
+
+      if (err) {
+        console.log('GET ERROR, playground:', err);
+        res.sendStatus(500);
+      }
+
+      console.log('Playground Report:', result.rows);
+      res.send(result.rows);
+    });
+  });
+});
 
 module.exports = router;
