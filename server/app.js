07c41967c874bfddc181bf2c331062a25147bc8c
kmaimn <kmai.mn@gmail.com>
2016-09-29 18:09:50 -0500

    started work on db queries

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var path = require('path');
 //define routes here;
 var reportRoute = require('./routes/reportRoute');
 var dataRoute = require('./routes/dataRoute');
+var federalRoute = require('./routes/federalRoute');
 
 app.use(bodyParser.json());
 
@@ -14,6 +15,7 @@ app.use(express.static(path.join(__dirname, './public')));
 //set app.use for route here;
 app.use('/reportRoute', reportRoute);
 app.use('/dataRoute', dataRoute);
+app.use('/federalRoute', federalRoute);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
