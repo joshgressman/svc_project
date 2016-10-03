1ce785b2fbb94028cc06c884519cf931fc5deff7
Tyler Rapson <rapson95@gmail.com>
2016-10-03 10:25:12 -0500

    nonVictim controller

--- server/public/scripts/controllers/nonVictimController.js
+++ server/public/scripts/controllers/nonVictimController.js
@@ -1,8 +1,36 @@
 myApp.controller('nonVictimController', ['$scope', '$http', '$location', function($scope, $http, $location){
 
-
-
-
+  $scope.form = {
+      counselor: '',
+      date: '',
+      sTime: '',
+      eTime: '',
+      callerName: '',
+      callerPhone: '',
+      callerZip: '',
+      county: '',
+      svcSource: '',
+      medical: '',
+      school: '',
+      govtSocial: '',
+      communityMember: '',
+      lawEnforcer: '',
+      legalSystem: '',
+      citySocial: '',
+      otherOraganization: '',
+      medicalReferral: '',
+      schoolReferral: '',
+      govtSocialReferral: '',
+      communityMemberReferral: '',
+      lawEnforcerReferral: '',
+      legalSystemReferral: '',
+      citySocialReferral: '',
+      otherOraganizationReferral: '',
+      advacacyLocation: '',
+      dispatched: '',
+      responded: '',
+      reason: '',
+    };
 
 ///**********END OF CONTROLLER***************************************///////
 }]);
