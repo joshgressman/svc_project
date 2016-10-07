9360ebe67862e77fc3ea666a33241347ac4ec0f3
Corey Peck <coreypeck@gmail.com>
2016-10-07 17:11:09 -0500

    fixed inperson radio buttons

--- server/public/scripts/controllers/dataEntryController.js
+++ server/public/scripts/controllers/dataEntryController.js
@@ -1,19 +1,20 @@
 myApp.controller('dataEntryController', ['$scope', '$http', '$location', 'loggedinFactory', function($scope, $http, $location, loggedinFactory) {
+    console.log("Data Entry Running FOOL");
+    $scope.check = false;
+    $scope.loggedinFactory = loggedinFactory;
+    $scope.thing = {};
 
-  $scope.check = false;
-  $scope.loggedinFactory = loggedinFactory;
-
-  $scope.isLoggedIn = function() {
-    loggedinFactory.isLoggedIn().then(function(response) {
-      console.log('The person logged in:', response);
-      console.log('the type of the person logged in:', response.user_type)
-      $scope.user = response;
-      if (response.user_type == 'admin') {
-        $scope.check = true;
-      }
-    });
-  }
-      $scope.oneAtATime = true;
+    $scope.isLoggedIn = function() {
+        loggedinFactory.isLoggedIn().then(function(response) {
+            console.log('The person logged in:', response);
+            console.log('the type of the person logged in:', response.user_type)
+            $scope.user = response;
+            if (response.user_type == 'admin') {
+                $scope.check = true;
+            }
+        });
+    }
+    $scope.oneAtATime = true;
     $scope.status = {
         isCustomHeaderOpen: false,
         isFirstOpen: true,
@@ -21,121 +22,138 @@ myApp.controller('dataEntryController', ['$scope', '$http', '$location', 'logged
     };
 
 
-  $scope.myFunction = function() {
-      window.print();
-  }
+    $scope.myFunction = function() {
+        window.print();
+    }
 
-  $scope.form = {
-    counselor: null,
-    date: null,
-    sTime: null,
-    eTime: null,
-    location: null,
-    county: null,
-    clientNumber: null,
-    zipCode: null,
-    victimType: '',
-    svcPrompt: null,
-    previousContact: null,
-    previousVisit: null,
-    transportation: null,
-    counseling: null,
-    supportGroup: null,
-    lawEnforcementInterview: null,
-    prosecutionRelatedAdvocacy: null,
-    courtAdvocacy: null,
-    assistOFP_HRO: null,
-    immigrationSupport: null,
-    interventionWithOthers: null,
-    forensicExamSupport: null,
-    accompanyMedicalAppt: null,
-    accompanyDentalAppt: null,
-    crisis_counseling: null,
-    infoAndReferral: null,
-    info_crimjustice: null,
-    other_emergency_justice: null,
-    safeAtHome: null,
-    emergencyFinancialAsst: null,
-    reparationsClaimAsst: null,
-    svcServices: null,
-    otherAgencyReferral: null,
-    otherServicesReferral: null,
-    adultSexAssault: null,
-    adultAbusedAsChild_family: null,
-    adultAbusedAsChild_other: null,
-    exposing: null,
-    minorCSA_family: null,
-    minorCSA_other: null,
-    obscenePhoneCall: null,
-    exploitation_trafficking: null,
-    sexualHarassment: null,
-    stalking: null,
-    internetRelated: null,
-    unknownViolence: null,
-    bullying: null,
-    childPorn: null,
-    domesticViolence: null,
-    elderAbuse: null,
-    teenDating: null,
-    sexualViolenceOther: null,
-    sexualViolenceOther_specify: null,
-    age: null,
-    gender: null,
-    trans: null,
-    orientation: null,
-    blind_visImpair: null,
-    physDisabled: null,
-    mentDisabled: null,
-    deafHardHearing: null,
-    devDisabled: null,
-    notDisabled: null,
-    unknownDisabled: null,
-    otherDisabled: null,
-    otherDisabled_specify: null,
-    ethnicBackground: null,
-    immigrantStatus: null,
-    homeless: null,
-    limitedEnglish: null,
-    veteran: null,
-    supported: null,
-    advocacyType: null,
-    multiple: null,
-    formId: null,
-    other_ethnicBackground: null,
-    other_immigrantStatus: null
-  }
+    $scope.form = {
+        counselor: null,
+        date: null,
+        sTime: null,
+        eTime: null,
+        location: null,
+        county: null,
+        clientNumber: null,
+        zipCode: null,
+        victimType: '',
+        svcPrompt: null,
+        previousContact: null,
+        previousVisit: null,
+        transportation: null,
+        counseling: null,
+        supportGroup: null,
+        lawEnforcementInterview: null,
+        prosecutionRelatedAdvocacy: null,
+        courtAdvocacy: null,
+        assistOFP_HRO: null,
+        immigrationSupport: null,
+        interventionWithOthers: null,
+        forensicExamSupport: null,
+        accompanyMedicalAppt: null,
+        accompanyDentalAppt: null,
+        crisis_counseling: null,
+        infoAndReferral: null,
+        info_crimjustice: null,
+        other_emergency_justice: null,
+        safeAtHome: null,
+        emergencyFinancialAsst: null,
+        reparationsClaimAsst: null,
+        svcServices: null,
+        otherAgencyReferral: null,
+        otherServicesReferral: null,
+        adultSexAssault: null,
+        adultAbusedAsChild_family: null,
+        adultAbusedAsChild_other: null,
+        exposing: null,
+        minorCSA_family: null,
+        minorCSA_other: null,
+        obscenePhoneCall: null,
+        exploitation_trafficking: null,
+        sexualHarassment: null,
+        stalking: null,
+        internetRelated: null,
+        unknownViolence: null,
+        bullying: null,
+        childPorn: null,
+        domesticViolence: null,
+        elderAbuse: null,
+        teenDating: null,
+        sexualViolenceOther: null,
+        sexualViolenceOther_specify: null,
+        age: null,
+        gender: null,
+        trans: null,
+        orientation: null,
+        blind_visImpair: null,
+        physDisabled: null,
+        mentDisabled: null,
+        deafHardHearing: null,
+        devDisabled: null,
+        notDisabled: null,
+        unknownDisabled: null,
+        otherDisabled: null,
+        otherDisabled_specify: null,
+        ethnicBackground: null,
+        immigrantStatus: null,
+        homeless: null,
+        limitedEnglish: null,
+        veteran: null,
+        supported: null,
+        advocacyType: null,
+        multiple: null,
+        formId: null,
+        other_ethnicBackground: null,
+        other_immigrantStatus: null
+    }
 
-  // var victimizationCount = [];
+    // var victimizationCount = [];
 
+    $scope.submitVictimForm = function() {
+          var standinObject = $scope.thing;
+          console.log(standinObject);
+            var thingArray = Object.getOwnPropertyNames(standinObject);
+            console.log(thingArray);
+            var potato = {};
+            thingArray.forEach(function(propertyName) {
+                potato[propertyName] = standinObject[propertyName];
+                console.log(standinObject);
+                console.log(potato);
+            });
+            var formArray = Object.getOwnPropertyNames(potato);
+            formArray.forEach(function(parameter) {
+                var parameterName = potato[parameter];
+                console.log(potato[parameter]);
+                console.log(parameterName);
+                $scope.form[parameterName] = true;
+            });
+            console.log($scope.form);
+            var data = $scope.form;
+            // var count = victimizationCount.length;
+            // console.log(count);
+            //formats input date into workable format;
+            data.date_entered = new Date();
 
-  $scope.submitVictimForm = function() {
-      var data = $scope.form;
-      // var count = victimizationCount.length;
-      // console.log(count);
-      //formats input date into workable format;
-      data.date_entered = new Date();
+            console.log('sending to server...', data);
+            $http.post('/dataRoute/victim', data).then(function(response) {
+                    console.log('success');
 
-      console.log('sending to server...', data);
-      $http.post('/dataRoute/victim', data).then(function(response) {
-        console.log('success');
+                },
+                function(response) {
+                    console.log('error');
+                    $scope.message = "Please try again."
+                });
+        }
+        ///Search & update victim and non victim form
+    $scope.table = {};
+    $scope.update = {}
 
-        },
-        function(response) {
-          console.log('error');
-          $scope.message = "Please try again."
-        });
-      }
-///Search & update victim and non victim form
-      $scope.table = {};
-      $scope.update = {}
-
-      $scope.searchUpdate = function (){
+    $scope.searchUpdate = function() {
         var data = {};
         var id = $scope.formId;
         var info = Object.getOwnPropertyNames($scope.table);
-        $scope[info[0]]= true;
-        if(info[0] == "phone"){
-          info[0] = "victim";
+        $scope[info[0]] = true;
+        if (info[0] == "phone") {
+            info[0] = "victim";
         }
         data.table = info[0];
         data.number = parseInt($scope.formId);
@@ -147,39 +165,40 @@ myApp.controller('dataEntryController', ['$scope', '$http', '$location', 'logged
         }).then(function(response) {
             // console.log("Get Success");
             // console.log(response);
-              $scope.update = response.data[0];
-              // console.log($scope.update);
-      });
+            $scope.update = response.data[0];
+            // console.log($scope.update);
+        });
     }
 
     ////////////UPDATE FORM /////////////////////////
 
-$scope.updateForm = function () {
-  var data = {}
-  // var info = Object.getOwnPropertyNames($scope.table);
-  var id = parseInt($scope.update.id);
-   data = $scope.update;
-  console.log("update id", id);
-  console.log("update data", data);
-  $http({
-      method: "PUT",
-      url: '/dataRoute/victim/' + id,
-      data: data
-  }).then(function(response) {
-      console.log("Get Success");
-      console.log(response);
-        // $scope.update = response.data[0];
-        console.log($scope.update);
-});
-}
+    $scope.updateForm = function() {
+        var data = {}
+            // var info = Object.getOwnPropertyNames($scope.table);
+        var id = parseInt($scope.update.id);
+        data = $scope.update;
+        console.log("update id", id);
+        console.log("update data", data);
+        $http({
+            method: "PUT",
+            url: '/dataRoute/victim/' + id,
+            data: data
+        }).then(function(response) {
+            console.log("Get Success");
+            console.log(response);
+            // $scope.update = response.data[0];
+            console.log($scope.update);
+        });
+    }
 
 
- ///////////////////////////////////////////////////
 
- ///////////DELETE FORM////////////////////////////
+    ///////////////////////////////////////////////////
+
+    ///////////DELETE FORM////////////////////////////
 
 
- ////////////////////////////////////////////////
+    ////////////////////////////////////////////////
 
     ///**********END OF CONTROLLER***************************************///////
 }]);
