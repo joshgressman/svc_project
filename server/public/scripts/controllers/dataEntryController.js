e2fda67d7511474f4d3c5fedacfeca0d0b94a6ac
joshgressman <joshgressman@gmail.com>
2016-10-10 13:25:12 -0500

    pull from kimmai

--- server/public/scripts/controllers/dataEntryController.js
+++ server/public/scripts/controllers/dataEntryController.js
@@ -25,6 +25,8 @@ myApp.controller('dataEntryController', ['$scope', '$http', '$location', 'logged
       window.print();
   }
 
+
+
   $scope.form = {
     counselor: null,
     date: null,
@@ -108,103 +110,221 @@ myApp.controller('dataEntryController', ['$scope', '$http', '$location', 'logged
   // var victimizationCount = [];
 
 
+  // $scope.submitVictimForm = function() {
+  //     var data = $scope.form;
+  //     // var count = victimizationCount.length;
+  //     // console.log(count);
+  //     //formats input date into workable format;
+  //     data.date_entered = new Date();
+  //
+  //     console.log('sending to server...', data);
+  //     $http.post('/dataRoute/victim', data).then(function(response) {
+  //       console.log('success');
+  //
+  //      $scope.form = {
+  //        counselor: null,
+  //        date: null,
+  //        sTime: null,
+  //        eTime: null,
+  //        location: null,
+  //        county: null,
+  //        clientNumber: null,
+  //        zipCode: null,
+  //        victimType: '',
+  //        svcPrompt: null,
+  //        previousContact: null,
+  //        previousVisit: null,
+  //        transportation: null,
+  //        counseling: null,
+  //        supportGroup: null,
+  //        lawEnforcementInterview: null,
+  //        prosecutionRelatedAdvocacy: null,
+  //        courtAdvocacy: null,
+  //        assistOFP_HRO: null,
+  //        immigrationSupport: null,
+  //        interventionWithOthers: null,
+  //        forensicExamSupport: null,
+  //        accompanyMedicalAppt: null,
+  //        accompanyDentalAppt: null,
+  //        crisis_counseling: null,
+  //        infoAndReferral: null,
+  //        info_crimjustice: null,
+  //        other_emergency_justice: null,
+  //        safeAtHome: null,
+  //        emergencyFinancialAsst: null,
+  //        reparationsClaimAsst: null,
+  //        svcServices: null,
+  //        otherAgencyReferral: null,
+  //        otherServicesReferral: null,
+  //        adultSexAssault: null,
+  //        adultAbusedAsChild_family: null,
+  //        adultAbusedAsChild_other: null,
+  //        exposing: null,
+  //        minorCSA_family: null,
+  //        minorCSA_other: null,
+  //        obscenePhoneCall: null,
+  //        exploitation_trafficking: null,
+  //        sexualHarassment: null,
+  //        stalking: null,
+  //        internetRelated: null,
+  //        unknownViolence: null,
+  //        bullying: null,
+  //        childPorn: null,
+  //        domesticViolence: null,
+  //        elderAbuse: null,
+  //        teenDating: null,
+  //        sexualViolenceOther: null,
+  //        sexualViolenceOther_specify: null,
+  //        age: null,
+  //        gender: null,
+  //        trans: null,
+  //        orientation: null,
+  //        blind_visImpair: null,
+  //        physDisabled: null,
+  //        mentDisabled: null,
+  //        deafHardHearing: null,
+  //        devDisabled: null,
+  //        notDisabled: null,
+  //        unknownDisabled: null,
+  //        otherDisabled: null,
+  //        otherDisabled_specify: null,
+  //        ethnicBackground: null,
+  //        immigrantStatus: null,
+  //        homeless: null,
+  //        limitedEnglish: null,
+  //        veteran: null,
+  //        supported: null,
+  //        advocacyType: null,
+  //        multiple: null,
+  //        formId: null,
+  //        other_ethnicBackground: null,
+  //        other_immigrantStatus: null
+  //      }
+  //      $scope.message = "Form Submited."
+  //       },
+  //       function(response) {
+  //         console.log('error');
+  //         $scope.message = "Please try again."
+  //       });
+  //     }
   $scope.submitVictimForm = function() {
-      var data = $scope.form;
-      // var count = victimizationCount.length;
-      // console.log(count);
-      //formats input date into workable format;
-      data.date_entered = new Date();
+   if ($scope.form.date == null) {
+       $scope.message = "Please enter a date before submitting your request.";
+   } else {
+       var standinObject = $scope.thing;
+       console.log(standinObject);
+       var thingArray = Object.getOwnPropertyNames(standinObject);
+       console.log(thingArray);
+       var potato = {};
+       thingArray.forEach(function(propertyName) {
+           potato[propertyName] = standinObject[propertyName];
+           console.log(standinObject);
+           console.log(potato);
+       });
+       var formArray = Object.getOwnPropertyNames(potato);
+       formArray.forEach(function(parameter) {
+           var parameterName = potato[parameter];
+           console.log(potato[parameter]);
+           console.log(parameterName);
+           $scope.form[parameterName] = true;
+       });
+     var data = $scope.form;
+     // var count = victimizationCount.length;
+     // console.log(count);
+     //formats input date into workable format;
+     data.date_entered = new Date();
 
-      console.log('sending to server...', data);
-      $http.post('/dataRoute/victim', data).then(function(response) {
-        console.log('success');
+     console.log('sending to server...', data);
+     $http.post('/dataRoute/victim', data).then(function(response) {
+       console.log('success');
 
-       $scope.form = {
-         counselor: null,
-         date: null,
-         sTime: null,
-         eTime: null,
-         location: null,
-         county: null,
-         clientNumber: null,
-         zipCode: null,
-         victimType: '',
-         svcPrompt: null,
-         previousContact: null,
-         previousVisit: null,
-         transportation: null,
-         counseling: null,
-         supportGroup: null,
-         lawEnforcementInterview: null,
-         prosecutionRelatedAdvocacy: null,
-         courtAdvocacy: null,
-         assistOFP_HRO: null,
-         immigrationSupport: null,
-         interventionWithOthers: null,
-         forensicExamSupport: null,
-         accompanyMedicalAppt: null,
-         accompanyDentalAppt: null,
-         crisis_counseling: null,
-         infoAndReferral: null,
-         info_crimjustice: null,
-         other_emergency_justice: null,
-         safeAtHome: null,
-         emergencyFinancialAsst: null,
-         reparationsClaimAsst: null,
-         svcServices: null,
-         otherAgencyReferral: null,
-         otherServicesReferral: null,
-         adultSexAssault: null,
-         adultAbusedAsChild_family: null,
-         adultAbusedAsChild_other: null,
-         exposing: null,
-         minorCSA_family: null,
-         minorCSA_other: null,
-         obscenePhoneCall: null,
-         exploitation_trafficking: null,
-         sexualHarassment: null,
-         stalking: null,
-         internetRelated: null,
-         unknownViolence: null,
-         bullying: null,
-         childPorn: null,
-         domesticViolence: null,
-         elderAbuse: null,
-         teenDating: null,
-         sexualViolenceOther: null,
-         sexualViolenceOther_specify: null,
-         age: null,
-         gender: null,
-         trans: null,
-         orientation: null,
-         blind_visImpair: null,
-         physDisabled: null,
-         mentDisabled: null,
-         deafHardHearing: null,
-         devDisabled: null,
-         notDisabled: null,
-         unknownDisabled: null,
-         otherDisabled: null,
-         otherDisabled_specify: null,
-         ethnicBackground: null,
-         immigrantStatus: null,
-         homeless: null,
-         limitedEnglish: null,
-         veteran: null,
-         supported: null,
-         advocacyType: null,
-         multiple: null,
-         formId: null,
-         other_ethnicBackground: null,
-         other_immigrantStatus: null
-       }
-       $scope.message = "Form Submited."
-        },
-        function(response) {
-          console.log('error');
-          $scope.message = "Please try again."
-        });
+      $scope.form = {
+        counselor: null,
+        date: null,
+        sTime: null,
+        eTime: null,
+        location: null,
+        county: null,
+        clientNumber: null,
+        zipCode: null,
+        victimType: '',
+        svcPrompt: null,
+        previousContact: null,
+        previousVisit: null,
+        transportation: null,
+        counseling: null,
+        supportGroup: null,
+        lawEnforcementInterview: null,
+        prosecutionRelatedAdvocacy: null,
+        courtAdvocacy: null,
+        assistOFP_HRO: null,
+        immigrationSupport: null,
+        interventionWithOthers: null,
+        forensicExamSupport: null,
+        accompanyMedicalAppt: null,
+        accompanyDentalAppt: null,
+        crisis_counseling: null,
+        infoAndReferral: null,
+        info_crimjustice: null,
+        other_emergency_justice: null,
+        safeAtHome: null,
+        emergencyFinancialAsst: null,
+        reparationsClaimAsst: null,
+        svcServices: null,
+        otherAgencyReferral: null,
+        otherServicesReferral: null,
+        adultSexAssault: null,
+        adultAbusedAsChild_family: null,
+        adultAbusedAsChild_other: null,
+        exposing: null,
+        minorCSA_family: null,
+        minorCSA_other: null,
+        obscenePhoneCall: null,
+        exploitation_trafficking: null,
+        sexualHarassment: null,
+        stalking: null,
+        internetRelated: null,
+        unknownViolence: null,
+        bullying: null,
+        childPorn: null,
+        domesticViolence: null,
+        elderAbuse: null,
+        teenDating: null,
+        sexualViolenceOther: null,
+        sexualViolenceOther_specify: null,
+        age: null,
+        gender: null,
+        trans: null,
+        orientation: null,
+        blind_visImpair: null,
+        physDisabled: null,
+        mentDisabled: null,
+        deafHardHearing: null,
+        devDisabled: null,
+        notDisabled: null,
+        unknownDisabled: null,
+        otherDisabled: null,
+        otherDisabled_specify: null,
+        ethnicBackground: null,
+        immigrantStatus: null,
+        homeless: null,
+        limitedEnglish: null,
+        veteran: null,
+        supported: null,
+        advocacyType: null,
+        multiple: null,
+        formId: null,
+        other_ethnicBackground: null,
+        other_immigrantStatus: null
       }
+      $scope.message = "Form Submited."
+       },
+       function(response) {
+         console.log('error');
+         $scope.message = "Please try again."
+       });
+     }
+   }
 ///Search & update victim and non victim form
       $scope.table = {};
       $scope.update = {}
@@ -277,6 +397,7 @@ $scope.deleteForm = function () {
   }).then(function(response) {
       console.log("DELETE Success");
       console.log(response);
+
         // $scope.update = response.data[0];
         // console.log($scope.update);
 
--- server/public/scripts/controllers/nonVictimController.js
+++ server/public/scripts/controllers/nonVictimController.js
@@ -51,7 +51,41 @@ $scope.nonVictimInfo = {};
         console.log('sending to server non vict data', data);
         $http.post('/dataRoute/nonvictim', data).then(function(response){
           console.log('success');
-          // $scope.formId ++;
+
+          $scope.form = {
+                counselor: null,
+                date: null,
+                sTime: null,
+                eTime: null,
+                callerName: null,
+                callerPhone: null,
+                callerZip: null,
+                county: null,
+                callerType:null,
+                svcSource: null,
+                medical: null,
+                school: null,
+                govtSocial: null,
+                communityMember: null,
+                lawEnforcer: null,
+                legalSystem: null,
+                citySocial: null,
+                otherOraganization: null,
+                medicalReferral: null,
+                schoolReferral: null,
+                govtSocialReferral: null,
+                communityMemberReferral: null,
+                lawEnforcerReferral: null,
+                legalSystemReferral: null,
+                citySocialReferral: null,
+                otherOraganizationReferral: null,
+                advocacyLocation: null,
+                request: null,
+                dispatched: null,
+                responded: null,
+                reason: null,
+                medicalAdvocacyRequest: null,
+              };
         },
         function(response){
           console.log('error');
